c4a3c4f60b2742be46b0898e4c3fdd2c699121ea
Chady <chadygainz@gmail.com>
2017-08-27 13:30:58 -0700

    initial commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class CameraFollow : MonoBehaviour {
+	// The thing which this camera follows.
+	public GameObject Target;
+
+	// How far the camera is above the target
+	public float Distance = 5;
+
+	private void Update() {
+		this.transform.position = Target.transform.position;
+		this.transform.position = this.transform.position + new Vector3(0, Distance, 0);
+	}
+}
